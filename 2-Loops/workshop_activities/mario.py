f7fada2794f299df71a7b578fafbb3394086ca4e
Xavier P <126206539+TheWebsiteMan@users.noreply.github.com>
2025-02-17 00:34:44 +0000

    Bug fixes for workshop activities

--- 2-Loops/workshop_activities/mario.py
+++ 2-Loops/workshop_activities/mario.py
@@ -1,13 +1,16 @@
 def main():
+    """Main function"""
     print_square(3)
 
 
 def print_square(size):
-    for i in range(size):
+    """Function printing square"""
+    for _ in range(size):
         print_row(size)
 
 
 def print_row(width):
+    """Function printing row"""
     print("#" * width)
 
 
