7325948bd2df0519b512109b15b7ced3e5a5d3ce
Xavier P <126206539+TheWebsiteMan@users.noreply.github.com>
2025-02-17 00:30:50 +0000

    Upload completed workshop activities

---
+++ 2-Loops/workshop_activities/hogwarts.py
@@ -0,0 +1,9 @@
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",  
+}
+
+for student, house in students.items():
+    print(student, house, sep=", ")
